6d2524eb0191bcfb5fbab364af94673aa507be70
Nishanth Mankame <sparticlenish@gmail.com>
2017-09-08 20:45:39 -0400

    Fixed Hello World class error

---
+++ src/Variables_SKmod2.java
@@ -0,0 +1,42 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Variables_SKmod2
+{
+    public static void main ( String[] args )
+    {
+		/* 1. Declare one or more variables of each of the
+		   following data types: int double String char
+
+		   2. Initialize each variable with a suitable value.
+
+		   3. Complete the 'print' section in order to generate
+		    output that conforms to the sample output.   */
+
+        //integer variables here
+
+
+
+        //decimal variables here
+
+
+        //other types here. One is done for you.
+        String schoolName = "Sharon High School";
+        String myname = "Nishanth Mankame" ;
+        String date = "9/8/17";
+        int numbervar = 101;
+        //Complete this section to output your information
+        System.out.println("//////////////////////////////////");
+        System.out.println(myname + "        "+ date);
+        System.out.println("* CompSci at " + schoolName + "  *");
+        System.out.println(numbervar);
+        System.out.println("*                                *");
+
+
+    }
+}
