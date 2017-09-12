5badc98e858fb75af6ef974473d0691e2d528c8e
nishsticks <sparticlenish@gmail.com>
2017-09-12 11:43:33 -0400

    Added Time class

---
+++ src/Time.java
@@ -0,0 +1,24 @@
+/*
+ *   Replace this comment with a descriptive comment.
+ */
+
+
+public class Time		  // a general Java outline.
+{
+    public static void main(String args[])
+    {
+        // Define and initialize your variables here.
+    int hours;
+    int minutes;
+    int seconds;
+
+
+
+
+        // Write the main body of your program below this line
+
+
+
+
+    }
+}
