9f78fb5f61c41557c5f0a1a38b15ee28a1edcc3b
Nishanth Mankame <sparticlenish@gmail.com>
2017-09-11 23:54:35 -0400

    More updates

--- src/Hello_world.java
+++ src/Hello_world.java
@@ -2,6 +2,10 @@ public class Hello_world {
 
     public static void main( String[] args){
         System.out.println("Hello World");
+        String name = "Nishanth";
+        String age = "17";
+
+
 
 
 
