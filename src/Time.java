e015d10e8316ecaac13912d0af5d02d7ac6da30d
Nishanth Mankame <sparticlenish@gmail.com>
2017-09-12 23:43:53 -0400

    Finished Time Class

--- src/Time.java
+++ src/Time.java
@@ -8,15 +8,29 @@ public class Time		  // a general Java outline.
     public static void main(String args[])
     {
         // Define and initialize your variables here.
-    int hours;
-    int minutes;
-    int seconds;
+    double hours = 23; // Insert current time in hours and seconds in 24hr format
+    double minutes = 8;
+    System.out.println(System.currentTimeMillis());
+
+    double secMidnight = (hours * 60*60) + (minutes * 60); // Calculates the amount of seconds since midnight
+
+
+    double secRemain = (24*60*60) - secMidnight; // Calculates the amount of seconds that remain in the day
+
+
+
+    double percentDay = (secMidnight / (24*60*60)) * 100;  //Calculates the percent of day that has passed
+
 
 
 
 
         // Write the main body of your program below this line
+        System.out.println("The number of seconds since midnight is:" + " " + secMidnight);
 
+        System.out.println("The number of seconds remaining in the day are:" + " " + secRemain);
+
+        System.out.println("The percentage of day that has passed is:" + " " + percentDay + "%");
 
 
 
