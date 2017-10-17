198671ed9f4840bde09d6eda0da31432e6398b4e
nmankame2018 <sparticlenish@gmail.com>
2017-10-17 09:00:53 -0400

    Finished the Line class

---
+++ src/line/LineRunner.java
@@ -0,0 +1,27 @@
+package src.line;//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class LineRunner
+{
+	public static void main( String[] args )
+   {
+		//add test cases		
+		Line myLine = new Line(5, 4, 1, 2);
+
+
+		myLine.calculateSlope();
+		myLine.print();
+
+
+
+
+
+
+
+	}
+}
