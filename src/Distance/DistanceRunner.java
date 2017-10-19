def6f5587072ac44652c63a4808afa6aef963d57
nmankame2018 <sparticlenish@gmail.com>
2017-10-19 11:43:42 -0400

    almost finished with labs

---
+++ src/Distance/DistanceRunner.java
@@ -0,0 +1,25 @@
+package src.Distance;//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+//import java.util.Scanner;
+//import static java.lang.System.*;
+//import static java.lang.Math.*;
+
+public class DistanceRunner
+{
+	public static void main( String[] args )
+	{
+		//add test cases
+		Distance myDistance = new Distance(5, 4, 3, 2);
+		
+		myDistance.calcDistance();
+		myDistance.print();
+		
+			
+	}
+}
---
+++ src/Triangle/Triangle.java
@@ -0,0 +1,66 @@
+package src.Triangle;//(c) A+ Computer Science
+// www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner; 
+import java.lang.Math.*;
+
+public class Triangle
+{
+	private int sideA, sideB, sideC;
+	private double perimeter;
+	private double theArea;
+
+	public Triangle()
+	{
+	   setSides(0,0,0);
+	   perimeter=0;
+	   theArea=0;
+	}
+
+	public Triangle(int a, int b, int c)
+	{
+
+		setSides(a, b, c);
+
+
+	}
+
+
+	public void setSides(int a, int b, int c)
+	{
+		sideA = a;
+		sideB = b;
+		sideC = c;
+
+
+	}
+
+	public void calcPerimeter( )
+	{
+
+		perimeter = sideA + sideB + sideC;
+
+
+	}
+
+	public void calcArea( )
+	{
+		double s;
+		s = perimeter/2;
+		theArea = Math.sqrt(s * (s-sideA) *(s - sideB) * (s - sideC));
+
+
+	}
+
+	public void print( )
+	{
+
+
+		//System.out.printf("The perimeter is: %d\n The area is: %d", perimeter, theArea);
+	}
+}
