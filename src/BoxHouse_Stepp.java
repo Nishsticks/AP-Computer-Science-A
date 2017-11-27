4b359262731ea911206d837d41e233630ac2ca0b
nmankame2018 <sparticlenish@gmail.com>
2017-11-27 10:45:09 -0500

    Minor changes

---
+++ src/BoxHouse_Stepp.java
@@ -0,0 +1,23 @@
+public class BoxHouse_Stepp {
+	public static void main(String[] args) { 
+		buildRoof();
+		buildBody();
+
+	}
+
+	public static void buildRoof()
+	{
+		System.out.println("   ++");
+		System.out.println("  +  +");
+		System.out.println(" +    +");
+
+	}
+	public static void buildBody()
+	{
+		System.out.println("+------+");
+		System.out.println("|      |");
+		System.out.println("|      |");
+		System.out.println("+------+");
+	}
+
+}
---
+++ src/DEE_Harvard.java
@@ -0,0 +1,30 @@
+public class DEE_Harvard {
+	public static void main(String[] args) {
+		System.out.println(" -----");
+		drawD();
+		System.out.println(" -----");
+		System.out.println();
+		drawE();
+		System.out.println();
+		drawE();
+	}
+
+	public static void drawD()
+	{
+		System.out.println(" | \\");
+		System.out.println(" | |");
+		System.out.println(" | /");
+
+	}
+
+	public static void drawE()
+	{
+		System.out.println(" +-----");
+		System.out.println(" |");
+		System.out.println(" +----");
+		System.out.println(" |");
+		System.out.println(" +-----");
+
+	}
+
+}
