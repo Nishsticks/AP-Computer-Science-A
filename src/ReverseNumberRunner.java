7a8a2fa1e228e57bd340d411bc027f7ae97de560
nmankame2018 <sparticlenish@gmail.com>
2017-12-08 14:41:25 -0500

    Added in Revers digits lab

---
+++ src/ReverseNumberRunner.java
@@ -0,0 +1,34 @@
+//modified from (c) A+ Computer Science
+//
+//YOUR Name -
+import java.util.Scanner;
+
+public class ReverseNumberRunner
+{
+    public static void main( String args[] )
+    {
+        //add SCANNER to get test cases
+        Scanner input = new Scanner(System.in);
+        getReverse(input.nextInt());
+
+    }
+
+    public static int getReverse(int num)
+    {
+        int output=0;
+        while (num > 0){
+
+
+
+
+
+        }
+
+
+
+
+
+        return output;
+    }
+
+}
