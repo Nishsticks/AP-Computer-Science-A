5b094b6beabd359e45fe0ff7754b54218d58a7f5
nmankame2018 <sparticlenish@gmail.com>
2018-01-11 14:41:52 -0500

    added few files

---
+++ src/Stub.java
@@ -0,0 +1,31 @@
+/*
+ * Java shell
+ */
+package src;
+
+import java.util.Arrays;
+
+public class Stub		  // a general purpose stub.
+{
+    public static void main(String args[])
+    {
+
+        // Define and initialize your variables here.
+        for (int i = 1; i <=3; i++)
+        {
+            if (i==1)
+            {
+                int[] array1 = {0,1,2,3,4,5,6,7,8,9};
+                System.out.println(Arrays.toString(array1));
+            }
+
+            if (i==2)
+            {
+                int[] array2 = {0,-1,-2,-3,-5}
+            }
+        }
+
+
+        //Write your print statements below this line
+    }
+}
