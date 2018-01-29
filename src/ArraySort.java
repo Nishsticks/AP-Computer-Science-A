19bc0a303b831bb548036635d98fd8250f7a8d23
Nishanth Mankame <sparticlenish@gmail.com>
2018-01-29 00:14:45 -0500

    added few files

---
+++ src/ArraySort.java
@@ -0,0 +1,27 @@
+package src;
+
+public class ArraySort {
+
+    public static void main(String args[])
+    {
+
+        int[] arr = new int[] {9,6,3,7,5,8,1,2,0};
+        int minVal = 0;
+        int placeHold = 0;
+
+        for (int i = 0; i<arr.length; i++)
+        {
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+}
